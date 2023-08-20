f9e35e7978443494e976481320d246a3f8737cb9
tytwitchell <tytwitchell@gmail.com>
2023-08-20 19:20:51 -0400

    randomized dog profiles
    renders random dog profiles based on if they have been swiped yet

--- index.js
+++ index.js
@@ -3,47 +3,46 @@
 import { dogs } from './data.js'
 import { Dog } from './Dog.js'
 
-const dogObjects = dogs.map(dogData => 
-    new Dog (
-        dogData
-    )
-)
-
+const dogObjects = dogs.map(dogData => new Dog (dogData))
 
 const dislikeBtn = document.getElementById("dislike-btn")
 const likeBtn = document.getElementById("like-btn")
 
-
 dislikeBtn.addEventListener('click', () => handleBtnClick('nope-badge'))
 likeBtn.addEventListener('click', () => handleBtnClick('like-badge'))
+
+let currentDog = getRandomProfile();
+
+function getRandomProfile() {
+
+    const unSwipedDogs = dogObjects.filter(dog => !dog.hasBeenSwiped)
+
+    if (unSwipedDogs.length > 0) {
+        const randomDog = unSwipedDogs[Math.floor(Math.random()*unSwipedDogs.length)]
+        return randomDog
+
+    } else {
+        getMessageHtml();
+        setTimeout(render,500)
+    }
+}
 
 function handleBtnClick(action) {
 
     document.getElementById(action).classList.remove('hidden');
     
-    if (dogObjects.length) {
-        dogObjects.shift();
+    if (currentDog) {
+        currentDog.hasBeenSwiped = true;
+        currentDog = getRandomProfile();
         setTimeout(render,500)
     } else {
-        getMessageHtml();
-        setTimeout(render,500)
+        setTimeout(() => {
+            getMessageHtml();
+            render()
+        },500)
     }
 
 }
-
-function handleLikeBtn() {
-
-    document.getElementById("like-badge").classList.remove('hidden');
-
-    if (dogObjects.length) {
-        dogObjects.shift();
-    } else {
-        getMessageHtml();
-    }
-
-    setTimeout(render,500)
-}
-
 
 function getMessageHtml() {
 
@@ -55,12 +54,15 @@ function getMessageHtml() {
 }
 
 
-
 function render() {
     const dogContainer = document.getElementById("dog-container");
-    const renderedHtml = dogObjects.length > 0 ? dogObjects[0].getDogContainerHtml() 
-    : getMessageHtml();
-    dogContainer.innerHTML = renderedHtml;
+
+    if (currentDog) {
+        const renderedHtml = currentDog.getDogContainerHtml();
+        dogContainer.innerHTML = renderedHtml;
+    } else {
+        dogContainer.innerHTML = getMessageHtml();
+    }
 
 }
 
